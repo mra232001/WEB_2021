6821491854aa5b658eedd2fa556d778e173260fd
Lam Dinh Truong <dinhtruonglam001@gmail.com>
2022-01-05 19:47:40 +0300

    .

---
+++ src/main/java/com/example/web/entity/User.java
@@ -0,0 +1,33 @@
+package com.example.web.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "user")
+public class User {
+    @Id
+    @Column
+    private String user;
+
+    @Column
+    private String password;
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
