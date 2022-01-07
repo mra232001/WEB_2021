0e87041735b7654db16b8a4bb8cd99645097af8f
Lam Dinh Truong <dinhtruonglam001@gmail.com>
2022-01-07 13:55:59 +0300

    .

---
+++ IdeaProjects/web2021/src/main/java/com/example/web2021/Web2021Application.java
@@ -0,0 +1,13 @@
+package com.example.web2021;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Web2021Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Web2021Application.class, args);
+    }
+
+}
