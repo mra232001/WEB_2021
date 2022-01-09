6e00663b754ea45c5663362465221c789265c83c
Manh Do <domanhitmo@gmail.com>
2022-01-09 15:46:54 +0300

    .

--- src/main/java/com/example/web/controller/LoginController.java
+++ src/main/java/com/example/web/controller/LoginController.java
@@ -2,6 +2,7 @@ package com.example.web.controller;
 
 import com.example.web.entity.User;
 import com.example.web.service.LoginService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 @Controller
 public class LoginController {
 
+    @Autowired
     private LoginService loginService;
 
     public LoginController(LoginService loginService){
